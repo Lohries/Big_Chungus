f81a7f1d77192f0685e1f31e92b31ff19a44d09d
Patrick <lteloladeira@gmail.com>
2024-03-05 13:50:15 -0300

    done

--- First_Project/frontend/src/components/pages/Analyze.js
+++ First_Project/frontend/src/components/pages/Analyze.js
@@ -1,13 +1,20 @@
-import Footer from "../Footer"
-import Header from "../Header"
+import Footer from "../Footer";
+import Header from "../Header";
+
+import styles from "../CamDisplay.module.css";
+
 function Analyze_page() {
-    return (
-        <div>
-            <Header />
-            <Footer />
-        </div>
-    )
-
-
+  return (
+    <div>
+      <Header />
+     
+      <div className={styles.Display_out}>
+        <p>Resultados</p>
+      </div>
+      <input type="file" id="img-input-analyze" className={styles.input} />
+      <Footer />
+    </div>
+  );
 }
-export default Analyze_page
+
+export default Analyze_page;
