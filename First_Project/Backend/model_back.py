72a9f69ee22f89eb7737cea8790d74e0790c621b
Patrick <lteloladeira@gmail.com>
2024-03-06 17:50:12 -0300

    db-models

--- First_Project/Backend/model_back.py
+++ First_Project/Backend/model_back.py
@@ -1,31 +1,8 @@
 from deepface import DeepFace
 import cv2 as cv
+import mysql.connector
 
-def main():
-    print("Selecione o que fazer: ")
-    control_flow = input()
-    i = 0
-
-    if control_flow == '1':
-        frame = extraction()
-        
-        storing(frame, i)
-        print("Finalizado")
-        i = i + 1
-       
-
-    elif (control_flow == '2' and i >= 1):
-        verification = find()
-        print(verification)
-        
-        
-
-    #else:
-     #   analyze()
-
-
-
-def extraction():
+def extraction(i):
     capture = cv.VideoCapture(0)
     while True:
         success, frame = capture.read()
@@ -44,22 +21,61 @@ def extraction():
     
     cv.destroyAllWindows()
     capture.release()
+    
+    i = i + 1
     return frame
 
-def storing(frame, i):
-    cv.imwrite('img/frame{i}.jpg', frame)
-    print("Frame armazenado com sucesso!")
+
+def analyze():
+    pass
 
 
-def find():
-    frame_f = extraction()
-    df = DeepFace.find(frame_f, db_path='img')
-    return df
+def find(frame_f):
+    results = DeepFace.find(frame_f)
+    return results
 
 
-#def analyze():
- #   pass
+def Upload_File(frame_UF, cursor, Data_Base):
+    cv.imwrite(f"img/img{i}.jpg", frame_UF)
+    with open(frame_UF, "rb") as file:
+        Binary_data = file.read()
+    SQL_insert = "INSERT INTO Images (Photo) VALUES (%s)"
+    cursor.execute(SQL_insert, (Binary_data,))
+    Data_Base.commit()
 
 
-if __name__ == "__main__":
-    main()
+
+
+Data_Base = mysql.connector.connect(
+    host="localhost",
+    user="Lohries",
+    password="xxx",
+    database="Brain-Project"
+)
+
+cursor = Data_Base.cursor()
+
+query = """
+    CREATE TABLE IF NOT EXISTS Images (
+        id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        Photo MEDIUMBLOB NOT NULL
+    )
+"""
+cursor.execute(query)
+print("DataBase created !!!")
+
+i = 0
+while True:
+    control_flow = input("Select what to do (1)-Store (2)-Find (3)-Analyze): ")
+    if int(control_flow) == 1:
+        frame_CF = extraction(i)
+        Upload_File(frame_CF, cursor, Data_Base)
+        i += 1
+    elif int(control_flow) == 2:
+        frame_CF = extraction(i)
+        cv.imwrite(f"img/img{i}.jpg", frame_CF)
+        find(frame_CF)
+    elif int(control_flow) == 3:
+        analyze()
+    else:
+        break
