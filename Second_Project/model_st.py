1d4fd00d55a4bc079d26c5798436db6be93461b0
Patrick <lteloladeira@gmail.com>
2024-03-10 00:21:30 -0300

    points

--- Second_Project/model_st.py
+++ Second_Project/model_st.py
@@ -3,10 +3,33 @@ import cv2 as cv
 import os 
 
 
-#def data_base_analyze(data):
-directory = "img"
-for file in os.listdir(directory):
-    print(file)
+def data_base_analyze(data):
+    directory = "img"
+    lista2 = []
+    for i in enumerate(os.listdir(directory)):
+        
+        results = DeepFace.analyze(f"img/img{i+1}")
+        if results:
+            print("Age: ", results[0]["age"])
+            print("Gender: ", results[0]["gender"])
+            print("Emotion: ", results[0]["dominant_emotion"])
+            print("Race: ", results[0]["dominant_race"])
+            points = 0
+            if results[0]["dominant_emotion"] == data[0]:
+                points += 1
+            elif results[0]["age"] == data[1]:
+                points += 1
+            elif results[0]["gender"] == data[2]:
+                points += 1
+            elif results[0]["dominant_race"] == data[3]:
+                points += 1
+            
+            lista2.append(points)
+
+
+
+
+
 
 
 #def analyze_submit():
