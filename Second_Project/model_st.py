715adc3baf0499ce71be1f75fb7d067c2b987a97
Patrick <lteloladeira@gmail.com>
2024-03-09 21:49:30 -0300

    creating folder-iterating though

--- Second_Project/model_st.py
+++ Second_Project/model_st.py
@@ -1,12 +1,15 @@
 from deepface import DeepFace
 import cv2 as cv
+import os 
 
 
-def data_base_analyze():
-    pass
+#def data_base_analyze(data):
+directory = "img"
+for file in os.listdir(directory):
+    print(file)
 
 
-def analyze_submit():
-    pass
+#def analyze_submit():
+    #pass
 
 
