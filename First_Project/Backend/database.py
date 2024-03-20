f1b8b3d51a741f539c4f0e19e42ca7168c1cc48c
Patrick <lteloladeira@gmail.com>
2024-03-20 17:11:00 -0300

    db ok

--- First_Project/Backend/database.py
+++ First_Project/Backend/database.py
@@ -1,11 +1,27 @@
 from pymongo import MongoClient
 
-connection_string = "mongodb://admin:password@localhost:27017/?authSource=admin"
+connection_string = "mongodb://localhost:27017"
 
 client = MongoClient(connection_string)
 
 db_connection = client["meuBanco"]
 
-collection = db_connection.get_collection("Mingacollection")
+collection = db_connection.get_collection("minhaCollection")
 
 print(collection) 
+
+search_filter = { "hello" : "world"}
+search_filter2 = { "world" : "hello"}
+
+response1 = collection.find(search_filter)
+response2 = collection.find(search_filter2)
+
+
+
+
+
+
+collection.insert_one({ 
+    "Let's" : "fucking go",
+    "Numbers" : [144, 122, 99]
+})
